ad89a0e3fe1cbb785c614f42924d27e0d95ffb8e
SMazeikaite <52711596+SMazeikaite@users.noreply.github.com>
2023-12-04 08:48:50 +0200

    clean up

--- AOC_4-1.js
+++ AOC_4-1.js
@@ -6,16 +6,16 @@ fs.readFile("AOC_4_input.txt", (err, input) => {
     const cards = input.toString().split('\r\n');
     let result = 0;
 
-    cards.forEach((card) => {
+    for (const card of cards) {
         let [winningNumbers, ticketNumbers] = card.replace(/Card\s\d\:\s+/g, '').split('|');
         winningNumbers = winningNumbers.split(' ').filter(f => f !== '');
         ticketNumbers = ticketNumbers.split(' ').filter(f => f !== '');
-
-        let compare = (a1, a2) => a1.reduce((a, c) => a + a2.includes(c), 0);
-        
+    
+        const compare = (a1, a2) => a1.reduce((a, c) => a + a2.includes(c), 0);
         const matched = compare(winningNumbers, ticketNumbers);
-        result += matched > 1 ? (2 ** (matched - 1)) : 1;
-    });
+        
+        result += matched > 0 ? (2 ** (matched - 1)) : 0;
+    };
 
     console.log('result:', result);
 });
